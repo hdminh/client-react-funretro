318bc00737f1ac5645c327b115185550d7e8b2a1
hdminh <minhcine99@gmail.com>
2020-11-18 21:48:12 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,150 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { v4 as uuid } from 'uuid';
-import { getBoards } from './utils/api';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import TopBar from './components/TopBar';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import Home from './components/Home';
-
-const useStyle = makeStyles((theme) => ({
-  root: {
-    minHeight: '100vh',
-    background: '#ff99cc',
-    width: '100%',
-    overflowY: 'auto',
-  },
-  listContainer: {
-    display: 'flex',
-  },
-}));
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData();
-  }, [])
-  
-  const getData = async () => {
-    const data  = await getBoards();
-    const json = await data.json();
-    console.log(json);
-    setData(json);
-  }
-  const classes = useStyle();
-  const addMoreCard = (title, listId) => {
-    console.log(title, listId);
-
-    const newCardId = uuid();
-    const newCard = {
-      id: newCardId,
-      title,
-    };
-
-    const list = data.lists[listId];
-    list.cards = [...list.cards, newCard];
-
-    const newState = {
-      ...data,
-      lists: {
-        ...data.lists,
-        [listId]: list,
-      },
-    };
-    setData(newState);
-  };
-
-  const addMoreBoard = (title) => {
-    const newListId = uuid();
-    const newList = {
-      id: newListId,
-      title,
-      cards: [],
-    };
-    const newState = {
-      listIds: [...data.listIds, newListId],
-      lists: {
-        ...data.lists,
-        [newListId]: newList,
-      },
-    };
-    setData(newState);
-  };
-
-  const updateListTitle = (title, listId) => {
-
-    const list = data.lists[listId];
-    list.title = title;
-
-    const newState = {
-      ...data,
-      lists: {
-        ...data.lists,
-        [listId]: list,
-      },
-    };
-    setData(newState);
-  };
-
-  const updateBoardTitle = (title, boardId) => {
-    const list = data.lists[boardId];
-    list.title = title;
-  };
-
-  const onDragEnd = (result) => {
-    const { destination, source, draggableId, type } = result;
-    console.log('destination', destination, 'source', source, draggableId);
-
-    if (!destination) {
-      return;
-    }
-    if (type === 'list') {
-      const newListIds = data.listIds;
-      newListIds.splice(source.index, 1);
-      newListIds.splice(destination.index, 0, draggableId);
-      return;
-    }
-
-    const sourceList = data.lists[source.droppableId];
-    const destinationList = data.lists[destination.droppableId];
-    const draggingCard = sourceList.cards.filter(
-      (card) => card.id === draggableId
-    )[0];
-
-    if (source.droppableId === destination.droppableId) {
-      sourceList.cards.splice(source.index, 1);
-      destinationList.cards.splice(destination.index, 0, draggingCard);
-      const newSate = {
-        ...data,
-        lists: {
-          ...data.lists,
-          [sourceList.id]: destinationList,
-        },
-      };
-      setData(newSate);
-    } else {
-      sourceList.cards.splice(source.index, 1);
-      destinationList.cards.splice(destination.index, 0, draggingCard);
-
-      const newState = {
-        ...data,
-        lists: {
-          ...data.lists,
-          [sourceList.id]: sourceList,
-          [destinationList.id]: destinationList,
-        },
-      };
-      setData(newState);
-    }
-  };
-
   return (
     <Router>
       <div>
       <TopBar />
-      <Switch>
         <Route exact path="/" component={Home}/>
-      </Switch>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/signup" component={Signup}/>
       </div>
     </Router>
   );
